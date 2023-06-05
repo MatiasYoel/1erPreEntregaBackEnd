f4d001292126c7a10c1dfa28db2081abd57ebc58
MatiasYoel <fernandezmatiasyoel@gmail.com>
2023-06-05 11:37:44 -0300

    entrega clase 19

---
+++ src/config/configServer.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const url = 'mongodb+srv://fernandezmatiasyoel:VaULHrb41H2K9fje@cluster0.3aqi2wj.mongodb.net/?retryWrites=true&w=majority'
+
+
+const connectToDB = () => {
+    try {
+        mongoose.connect(url)
+        console.log('connected to DB e-commerce')
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default connectToDB
+
