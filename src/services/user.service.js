ff1945115c256f6658b4a98a0b7564999df4f391
MatiasYoel <fernandezmatiasyoel@gmail.com>
2023-08-16 23:50:00 -0300

    entregable clase 37

---
+++ src/services/user.service.js
@@ -0,0 +1,27 @@
+export default class UserService{
+    constructor(dao){
+        this.dao = dao;
+    }
+    getUsersService = (params) => {
+        return this.dao.getUsers(params)
+    }
+    getUsersByIdService = (id) => {
+        return this.dao.getUsersById(id)
+    }
+    getUsersByEmailService = (email) => {
+        return this.dao.getUsersByEmail(email)
+    }
+    validateUserService = (email) => {
+        return this.dao.validateUser(email)
+    }
+    createUserService = (user) => {
+        return this.dao.createUser(user)
+    }
+    addCartService = (cart) => {
+        return this.dao.addCart(cart)
+    }
+
+    changeUserService = (id, role) => {
+        return this.dao.changeRole(id, role)
+    }
+}
